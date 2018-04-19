e397346d7c82bb4dee7aaf9996ebdd3670668b21
Roberto Esposito <roberto.esposito@unito.it>
2018-04-19 15:43:19 +0200

    Better output

--- code/fair_networks.py
+++ code/fair_networks.py
@@ -15,15 +15,15 @@ y = tf.placeholder(tf.float32, shape=[None, 2], name="y")
 is_training = tf.placeholder(tf.bool, None)
 
 with tf.name_scope("layer-1"):
-    l1 = tf.layers.dense(x, 25, activation=tf.nn.sigmoid, kernel_initializer = tf.random_normal_initializer())
+    l1 = tf.layers.dense(x, 20, activation=tf.nn.sigmoid, kernel_initializer = tf.random_normal_initializer())
     # l1 = tf.layers.dropout(l1, rate = 0.5, training = is_training)
 
-with tf.name_scope("layer-2"):
-    l2 = tf.layers.dense(l1, 25, activation=tf.nn.sigmoid, kernel_initializer = tf.random_normal_initializer())
-    # l2 = tf.layers.dropout(l2, rate = 0.5, training = is_training)
+# with tf.name_scope("layer-2"):
+#     l2 = tf.layers.dense(l1, 25, activation=tf.nn.sigmoid, kernel_initializer = tf.random_normal_initializer())
+#     # l2 = tf.layers.dropout(l2, rate = 0.5, training = is_training)
 
 with tf.name_scope("out"):
-    out = tf.layers.dense(l2, 2, activation=tf.nn.sigmoid, kernel_initializer = tf.random_normal_initializer())
+    out = tf.layers.dense(l1, 2, activation=tf.nn.sigmoid, kernel_initializer = tf.random_normal_initializer())
 
 with tf.name_scope("loss"):
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=out))
@@ -52,10 +52,14 @@ session = tf.Session()
 init = tf.global_variables_initializer()
 session.run(init)
 
+print("%6s|%6s|%10s|%10s|%10s|%10s" % ("epoch", "it", "acc train", "acc test", "loss train", "loss test"))
+count = 0
+
 for _ in range(NUM_EPOCHS):
     session.run(train_it.initializer)
     while True:
         try:
+            count += 1
             xs, ys = session.run(train_next)
             session.run(train_step, feed_dict = { x:xs, y:ys, is_training: True } )
         except tf.errors.OutOfRangeError:
@@ -63,10 +67,11 @@ for _ in range(NUM_EPOCHS):
 
     last_epoch += 1
 
-    loss_val = session.run(loss, feed_dict={x: test_xs, y: test_ys, is_training: False})
-    accuracy_val = session.run(class_accuracy_op, feed_dict={x: test_xs, y: test_ys, is_training: False})
-    print(" test results -- epoch: %3d\taccuracy: %2.5f\tloss: %2.5f" % (last_epoch, accuracy_val, loss_val))
+    loss_train_val = session.run(loss, feed_dict={x: train_xs, y: train_ys, is_training: False})
+    accuracy_train_val = session.run(class_accuracy_op, feed_dict={x: train_xs, y: train_ys, is_training: False})
 
-    loss_val = session.run(loss, feed_dict={x: train_xs, y: train_ys, is_training: False})
-    accuracy_val = session.run(class_accuracy_op, feed_dict={x: train_xs, y: train_ys, is_training: False})
-    print("train results -- epoch: %3d\taccuracy: %2.5f\tloss: %2.5f" % (last_epoch, accuracy_val, loss_val))
+    loss_test_val = session.run(loss, feed_dict={x: test_xs, y: test_ys, is_training: False})
+    accuracy_test_val = session.run(class_accuracy_op, feed_dict={x: test_xs, y: test_ys, is_training: False})
+
+
+    print("%6d|%6d|%2.8f|%2.8f|%2.8f|%2.8f" % (last_epoch, count, accuracy_train_val, accuracy_test_val, loss_train_val, loss_test_val))
