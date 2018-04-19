38a19a63efe3b58e0e827bcb764baa7a264c27dc
Roberto Esposito <roberto.esposito@unito.it>
2018-04-19 14:18:30 +0200

    code cleanup
    switched to tf.layers api for building the layers. Added dropout to the
    hidden layer (currently commented).

--- code/fair_networks.py
+++ code/fair_networks.py
@@ -5,25 +5,21 @@ import tensorflow as tf
 import time
 import adult_dataset as ds
 
-NUM_EPOCHS = 100
-
-def fully_connected_layer(input_layer, num_out_nodes):
-    num_in_nodes = input_layer.get_shape().as_list()[1]
-    W = tf.Variable(tf.random_normal([num_in_nodes, num_out_nodes],0,1))
-    b = tf.Variable(tf.random_normal([num_out_nodes]))
-    return tf.sigmoid(tf.matmul(input_layer, W) + b)
+NUM_EPOCHS = 1000
 
 
 # Building model
 
 x = tf.placeholder(tf.float32, shape=[None, 108], name="x")
 y = tf.placeholder(tf.float32, shape=[None, 2], name="y")
+is_training = tf.placeholder(tf.bool, None)
 
 with tf.name_scope("layer-1"):
-    l1 = fully_connected_layer(x, 10)
+    l1 = tf.layers.dense(x, 10, activation=tf.nn.sigmoid, kernel_initializer = tf.random_normal_initializer())
+    # l1 = tf.layers.dropout(l1, rate = 0.5, training = is_training)
 
 with tf.name_scope("out"):
-    out = fully_connected_layer(l1, 2)
+    out = tf.layers.dense(l1, 2, activation=tf.nn.sigmoid, kernel_initializer = tf.random_normal_initializer())
 
 with tf.name_scope("loss"):
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=out))
@@ -38,7 +34,7 @@ train_step = tf.train.GradientDescentOptimizer(3.0).minimize(loss)
 
 dataset = ds.AdultDataset()
 trainset = dataset.traindata().batch(100)
-testset = dataset.testdata().batch(100)
+testset = dataset.testdata().batch(1000)
 
 train_it = trainset.make_initializable_iterator()
 train_next = train_it.get_next()
@@ -57,7 +53,7 @@ for _ in range(NUM_EPOCHS):
     while True:
         try:
             xs, ys = session.run(train_next)
-            session.run(train_step, feed_dict = { x:xs, y:ys } )
+            session.run(train_step, feed_dict = { x:xs, y:ys, is_training: True } )
         except tf.errors.OutOfRangeError:
           break
 
@@ -66,6 +62,6 @@ for _ in range(NUM_EPOCHS):
     session.run(test_it.initializer)
     test_xs, test_ys = session.run(test_next)
 
-    loss_val = session.run(loss, feed_dict={x: test_xs, y: test_ys})
-    accuracy_val = session.run(class_accuracy_op, feed_dict={x: test_xs, y: test_ys})
+    loss_val = session.run(loss, feed_dict={x: test_xs, y: test_ys, is_training: False})
+    accuracy_val = session.run(class_accuracy_op, feed_dict={x: test_xs, y: test_ys, is_training: False})
     print("epoch: %3d\taccuracy: %2.5f\tloss: %2.5f" % (last_epoch, accuracy_val, loss_val))
