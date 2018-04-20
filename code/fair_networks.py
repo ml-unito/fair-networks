1fdc2fee5f8fafbb6fc9fbfa5972d4ed25ad0d19
Roberto Esposito <roberto.esposito@unito.it>
2018-04-20 10:26:51 +0200

    Additional refactoring

--- code/fair_networks.py
+++ code/fair_networks.py
@@ -9,21 +9,19 @@ NUM_EPOCHS = 1000
 
 # Building model
 
-def build_model():
+def build_model(layers, optimizer):
     x = tf.placeholder(tf.float32, shape=[None, 108], name="x")
     y = tf.placeholder(tf.float32, shape=[None, 2], name="y")
     is_training = tf.placeholder(tf.bool, None)
+    in_layer = x
 
-    with tf.name_scope("layer-1"):
-        l1 = tf.layers.dense(x, 100, activation=tf.nn.sigmoid, kernel_initializer = tf.random_normal_initializer())
-        # l1 = tf.layers.dropout(l1, rate = 0.5, training = is_training)
-
-    # with tf.name_scope("layer-2"):
-    #     l2 = tf.layers.dense(l1, 25, activation=tf.nn.sigmoid, kernel_initializer = tf.random_normal_initializer())
-    #     # l2 = tf.layers.dropout(l2, rate = 0.5, training = is_training)
+    for index,layer in enumerate(layers):
+        num_nodes, activation, initializer = layer
+        with tf.name_scope("layer-%d" % (index+1)):
+            in_layer = tf.layers.dense(in_layer, num_nodes, activation=activation, kernel_initializer = initializer())
 
     with tf.name_scope("out"):
-        out = tf.layers.dense(l1, 2, activation=tf.nn.sigmoid, kernel_initializer = tf.random_normal_initializer())
+        out = tf.layers.dense(in_layer, 2, activation=tf.nn.sigmoid, kernel_initializer = tf.random_normal_initializer())
 
     with tf.name_scope("loss"):
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=out))
@@ -34,11 +32,22 @@ def build_model():
         accuracy = tf.cast(tf.reduce_mean(correct_predictions), "float")
         accuracy_stat = tf.summary.scalar("Accuracy", accuracy)
 
-    train_step = tf.train.GradientDescentOptimizer(100.0).minimize(loss)
+    train_step = optimizer.minimize(loss)
 
     return (x,y,train_step,is_training,loss,accuracy)
 
-x,y,train_step,is_training,loss,accuracy = build_model()
+def eval_loss_and_accuracy(session, loss, accuracy, xs, ys):
+    loss_val = session.run(loss, feed_dict={x: xs, y: ys, is_training: False})
+    accuracy_val = session.run(accuracy, feed_dict={x: xs, y: ys, is_training: False})
+    return (loss_val, accuracy_val)
+
+# --------------------------------------------------------------------------------
+# main
+# --------------------------------------------------------------------------------
+
+x,y,train_step,is_training,loss,accuracy = build_model([
+    (10, tf.nn.sigmoid, tf.random_normal_initializer)],
+    tf.train.GradientDescentOptimizer(3.0))
 
 dataset = ds.AdultDataset()
 
@@ -57,6 +66,7 @@ init = tf.global_variables_initializer()
 session.run(init)
 
 print("%6s|%6s|%10s|%10s|%10s|%10s" % ("epoch", "it", "acc train", "acc test", "loss train", "loss test"))
+
 count = 0
 
 for _ in range(NUM_EPOCHS):
@@ -71,11 +81,9 @@ for _ in range(NUM_EPOCHS):
 
     last_epoch += 1
 
-    loss_train_val = session.run(loss, feed_dict={x: train_xs, y: train_ys, is_training: False})
-    accuracy_train_val = session.run(accuracy, feed_dict={x: train_xs, y: train_ys, is_training: False})
+    loss_train_val, accuracy_train_val = eval_loss_and_accuracy(session, loss, accuracy, train_xs, train_ys)
 
-    loss_test_val = session.run(loss, feed_dict={x: test_xs, y: test_ys, is_training: False})
-    accuracy_test_val = session.run(accuracy, feed_dict={x: test_xs, y: test_ys, is_training: False})
+    loss_test_val, accuracy_test_val = eval_loss_and_accuracy(session, loss, accuracy, test_xs, test_ys)
 
 
     print("%6d|%6d|%2.8f|%2.8f|%2.8f|%2.8f" % (last_epoch, count, accuracy_train_val, accuracy_test_val, loss_train_val, loss_test_val))
