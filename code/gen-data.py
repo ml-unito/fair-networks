31e50310757c4aa3563542dc0712c4d9994c18fa
Roberto Esposito <roberto.esposito@unito.it>
2018-05-24 15:40:29 +0200

    New gen-data script

--- code/gen-data.py
+++ code/gen-data.py
@@ -2,31 +2,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression as LR
+from sklearn.svm import SVC
 
 DS_SIZE = 10000
-xmu = [0, 0]
+xmu = [1, 1]
 
-xcorr = [[1,0.7],
-        [0.7,1]]
+xcorr = np.array([[1,0.7],
+                  [0.7,1]])
 
-zmu = [0, 0]
+zmu = [1, 1]
 zcorr = [[1,0.4],
          [0.4,1]]
 
 x = np.random.multivariate_normal(xmu,xcorr,DS_SIZE)
 
-s = np.random.randint(5, size=DS_SIZE).reshape(10000,1)
-z = np.array([np.random.multivariate_normal(np.multiply(zmu, si), zcorr) for si in s])
+s = np.random.randint(5, size=DS_SIZE)
+zmus = [np.multiply(zmu, si) for si in s]
+z = np.array([np.random.multivariate_normal(mu, zcorr) for mu in zmus])
 
-y = np.exp(15*x[:,0]/100) + -3*x[:,1]**5 + 100*s[:,0]**3 + 5*(z[:,0] - z[:,1])**6
-y = y / np.max(np.abs(y)) > 0.0295
+# y = np.exp(15*np.sin(x[:,0])) + -3*x[:,1]**5 + 100*s[:,0]**3 + 5*(z[:,0] - z[:,1])**6
+# y = y / np.max(np.abs(y)) > 0.001
 
-ds = np.concatenate([x, s, z], axis=1)
 
+
+y =  [np.random.multivariate_normal(xi * (si+1), xcorr * (si+1)) for xi,si in zip(x,s)]
+y = np.array([np.exp(x1 + x2) for x1, x2 in y])
+y = y > 368.5
+
+sum(y)
+
+ds = np.concatenate([x, s.reshape(10000,1), z], axis=1)
+ds = np.concatenate([x,s.reshape(10000,1)], axis=1)
+ds = np.concatenate([x,z],axis=1)
+ds = x
 
 df = pd.DataFrame(np.concatenate([ds, np.reshape(y, [10000,1])], axis=1) , columns=["x1", "x2", "s", "z1", "z2", "y"])
-df.to_csv("data/synth-full.csv", sep=",")
+df.to_csv("data/synth-full.csv", sep=",", index=False)
+
+svc = SVC()
+
+
+model = svc.fit(ds, s)
+sum((model.predict(ds) == s) == True) / len(ds)
+
+plt.scatter(x[:,1], z[:,1], c=y, marker='.')
+
 
 # df = pd.DataFrame(np.concatenate([testx, np.reshape(testy, [2000,1])], axis=1) , columns=["x1", "x2", "s", "z1", "z2", "y"])
 # df.to_csv("synth-test.csv", sep=",")
