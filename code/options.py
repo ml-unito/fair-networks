ce35e2b3fd363505ee5e67796eadcfdf398c2516
Roberto Esposito <roberto.esposito@unito.it>
2018-06-13 14:49:33 +0200

    Remove -r option and add -c option
    The new option forces the user to always specify the checkpoint name.
    The checkpoint then will be always overwritten during learning.

--- code/options.py
+++ code/options.py
@@ -5,6 +5,7 @@ from synth_dataset import SynthDataset
 import tensorflow as tf
 import argparse
 import textwrap
+import os
 
 class Schedule:
     def __init__(self, schedule_list):
@@ -34,38 +35,12 @@ class Schedule:
 
 class Options:
     def __init__(self):
-        # self.num_features = 108   # Adult
-        # self.num_features = 51     # Bank
-
-        self.epoch_start = 0
-        self.epoch_end = 10000
-        self.epochs = range(self.epoch_start, self.epoch_end)
         self.resume_learning = False
-
         self.epochs_per_save = 1000
 
         self.parse(sys.argv)
 
         self.exp_name = "%s_h%s_s%s_y%s" % (self.dataset_name, self.hidden_layers_specs, self.sensible_layers_specs, self.class_layers_specs)
-
-    def parse_epochs(self, epochs_str):
-        epochs_spec = epochs_str.split(':')
-        if len(epochs_spec) == 1:
-            self.epoch_end = int(epochs_spec[0])
-            self.epochs = range(self.epoch_start, self.epoch_end)
-
-            return
-
-        start,end = epochs_spec
-
-        if start != '':
-            self.epoch_start = int(start)
-            self.resume_learning = True
-
-        if end != '':
-            self.epoch_end = int(end)
-
-        self.epochs = range(self.epoch_start, self.epoch_end)
 
     def parse_layers(self, str):
         layers_specs = str.split(':')
@@ -117,13 +92,13 @@ class Options:
 
         datasets = { 'adult': AdultDataset, 'bank': BankMarketingDataset, 'synth': SynthDataset }
         parser = argparse.ArgumentParser(description=description,formatter_class=argparse.RawDescriptionHelpFormatter)
-        parser.add_argument('-r', '--resume', metavar="FILENAME.ckpt", default="", type=str, help="If specified the script will load the given checkpoint instead of building a new model")
-        parser.add_argument('dataset', choices=['adult', 'bank', 'synth'], help="dataset to be loaded")
+        parser.add_argument('-c', '--checkpoint', metavar="FILENAME.ckpt", required=True, type=str, help="Name of the checkpoint to be saved/loaded.")
         parser.add_argument('-H', '--hidden-layers', type=str, help='hidden layers specs', required=True)
         parser.add_argument('-S', '--sensible-layers', type=str, help='sensible network specs', required=True)
         parser.add_argument('-Y', '--class-layers', type=str, help='output network specs', required=True)
         parser.add_argument('-e', '--eval-stats', default=False, action='store_const', const=True, help='Evaluate all stats and print the result on the console (if set training options will be ignored)')
         parser.add_argument('-s', '--schedule', type=str, help="Specifies how to schedule training epochs (see the main description for more information.)")
+        parser.add_argument('dataset', choices=['adult', 'bank', 'synth'], help="dataset to be loaded")
         result = parser.parse_args()
 
         self.dataset_name = result.dataset
@@ -139,8 +114,8 @@ class Options:
         self.class_layers_specs = result.class_layers
         self.class_layers = self.parse_layers(result.class_layers)
 
-        self.resume_learning = result.resume != ""
-        self._model_fname = result.resume
+        self._model_fname = result.checkpoint
+        self.resume_learning = os.path.isfile(self.model_fname())
 
         print(self.hidden_layers)
         print(self.sensible_layers)
@@ -153,11 +128,8 @@ class Options:
 
         return self
 
-    def model_fname(self, epoch):
-        if self.resume_learning:
-            return self._model_fname
-
-        return "models/%s-epoch-%d.ckpt" % (self.exp_name, epoch)
+    def model_fname(self):
+        return "models/%s.ckpt" % (self._model_fname)
 
     def log_fname(self):
         return 'logdir/log_%s' % self.exp_name
