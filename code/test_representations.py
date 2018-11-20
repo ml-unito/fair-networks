181f7fe9d9aee40f24ce7b68ff998ced93950529
boborbt <boborbt@gmail.com>
2018-11-20 17:24:41 +0100

    Add some more statistic to test_representations

--- code/test_representations.py
+++ code/test_representations.py
@@ -3,25 +3,38 @@ import sklearn.model_selection as ms
 import sklearn.svm as svm
 import numpy as np
 import sys
+from sklearn.metrics import confusion_matrix
 
-
-df = pandas.read_csv(sys.argv[1])
+file = "representations/bank_representations.csv"
+df = pandas.read_csv(file)
 y_columns = df.filter(regex=("y.*")).as_matrix()
 s_columns = df.filter(regex=("s.*")).as_matrix()
 h_columns = df.filter(regex=("h.*")).as_matrix()
 
 h_train, h_test, s_train, s_test, y_train, y_test = ms.train_test_split(h_columns, s_columns, y_columns)
 
-svc_y = svm.SVC()
+svc_y = svm.SVC(kernel="linear")
 svc_y.fit(h_train, y_train[:,1])
 y_pred = svc_y.predict(h_test)
 
 print("y accuracy: " + str(1.0 - np.mean(np.abs(y_test[:,1] - y_pred))))
 
-svc_s = svm.SVC()
+confusion_matrix(y_pred, y_test[:,1])
+
+
+svc_s = svm.SVC(kernel="linear")
 s_train = np.argmax(s_train, axis=1)
 s_test = np.argmax(s_test, axis=1)
 svc_s.fit(h_train, s_train)
 s_pred = svc_s.predict(h_test)
 
 print("s accuracy:" + str(1.0 - np.mean(np.abs(s_test - s_pred))))
+
+print(confusion_matrix(s_test, s_pred))
+
+sum(s_test == s_pred) / len(s_test)
+
+sum(s_test == 0)
+sum(s_test == 1)
+sum(s_test == 2)
+sum(s_test == 1) / len(s_test)
