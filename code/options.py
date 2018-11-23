b2936050af034b4e4cfdc7f33b495009b43e46d4
boborbt <boborbt@gmail.com>
2018-11-23 15:47:43 +0100

    Improved comments in options.py

--- code/options.py
+++ code/options.py
@@ -13,6 +13,58 @@ from copy import copy, deepcopy
 from termcolor import colored
 from pathlib import Path
 
+PARAMS_DESCRIPTION = """\
+or:
+
+ fair_networks.py CONFIG_FILENAME [other options]
+
+or:
+
+ fair_networks.py
+
+If CONFIG_FILENAME is given, the options found therein are used as
+initial option values. Any option given on the command line overwrite
+the ones in the file.
+
+If no option is given CONFIG_FILENAME is assumed to be the file '.fn-config'
+in the current directory (if exists).
+
+The SCHEDULE option specifies a training procedure by enumerating the number of epochs
+that should be spent optimizing different parts of the network and how.
+The syntax is:
+    SCHEDULE -> mINT:cINT
+    INT -> {1..9}{0..9}*
+
+where 'm' and 'c' specify the network part to be trained:
+    - m [model], train the main model
+    - c [classifiers], train the attached classifiers (y and s)
+
+the integer after the 'm' option specifies the total number of epochs to be performed;
+the integer after the 'c' option specifies how much the sensible and y layer have to be trained
+at each iteration.
+
+example:
+    m100:c100        -- train the whole network for 10 epochs; for each batch train
+                        the sensible and y network for 100 iterations before using it to
+                        feedback the prediction to train model
+
+"*_LAYERS" options specify the composition of sub networks;
+syntax is:
+    LAYER -> [sl]?INT
+    LAYER -> [sl]?INT:LAYER
+
+where the integers are the number of hidden units in the layer being specified and the
+optional 's' or 'l' flags specify the activation unit to be used (s==sigmoid, l==linear,
+default=='s').
+
+examples:
+     10       -- a single layer with 10 sigmoid neurons
+     10:5:2   -- three layers with 10, 5, and 2 neuron respectively, allo sigmoid units
+     l5:s3    -- two layers, first has 5 linear units, the second has 3 sigmoid units
+
+NOTE: layers options CAN be omitted when the model is restored from a .ckpt file
+"""
+
 class ParseError(Exception):
     """Exception raised for errors in the command line options.
 
@@ -24,6 +76,14 @@ class ParseError(Exception):
         self.message = message
 
 class Schedule:
+    """Simplifies scheduling of tasks specified by the options
+
+    Attributes:
+        schedule_specs: string array containing the components of the schedule as specified
+            in the options.
+        num_epochs: number of epochs to be performed following this schedule
+        sub_nets_num_it: numbe of epochs for training the s and y subworks
+    """
     def __init__(self, schedule_str):
         self.schedule_specs = schedule_str.split(':')
         self.num_epochs = self.parse_schedule('m',self.schedule_specs[0])
@@ -36,6 +96,45 @@ class Schedule:
         return int(epoch_spec[1:])
 
 class Options:
+    """Handles the options given to the main script
+
+        See the definition of the PARAMS_DESCRIPTION constant for a description of some of
+        the options.
+
+        One important facet of this handling is that options will loaded from json files
+        when possible. Command line options can be used to overwrite values read from disk
+        or for using the program without a json option file.
+
+        self.try_load_opts and self.try_update_opts are the methods used to try loading the
+        options from file and to merge command line options with those read from file.
+
+        # Attributes
+        
+        After initializations a property will be set for each option succesfully recognised:
+
+        self.dataset_name: dataset name to be used
+        self.dataset_base_path: directory where dataset are stored
+        self.dataset: an object instantiated to a subclass of DatasetBase allowing
+            working with the given dataset
+
+        self.num_features: number of features for the input data (depends on the dataset chosen)
+        self.checkpoint_output: path name where to store checkpoints
+        self.resume_learning: boolean specifying if a checkpoint needs to be restored
+
+        self.hidden_layers: array of tuples specifying how to build the hidden layer
+        self.sensible_layers: array of tuples specifying how to build the sensible layer
+        self.class_layers: array of tuples specifying how to build the class layer
+
+        if result.schedule: array containing the schedule for the training of the network
+
+        self.eval_stats: true if the task is simply to evaluate stats and exit
+        self.eval_data_path: string representing the path where to store the representations built
+            by the current model (if != None no training is performed)
+        self.fairness_importance: numeric value representing how important the fairness constraint is
+
+        self.epochs_per_save: number of epochs to be performed before saving a new model. This is
+            used only epochs > 1000. Before this treshold a model is saved every 10 epochs.
+    """
     def __init__(self):
         self.resume_learning = False
         self.epochs_per_save = 1000
@@ -130,64 +229,8 @@ class Options:
         parsed_args.__dict__ = config_opts
         return parsed_args
 
-    def parse(self, argv):
-        description = """\
-        or:
-
-         fair_networks.py CONFIG_FILENAME [other options]
-
-        or:
-
-         fair_networks.py
-
-        If CONFIG_FILENAME is given, the options found therein are used as
-        initial option values. Any option given on the command line overwrite
-        the ones in the file.
-
-        If no option is given CONFIG_FILENAME is assumed to be the file '.fn-config'
-        in the current directory (if exists).
-
-        The SCHEDULE option specifies a training procedure by enumerating the number of epochs
-        that should be spent optimizing different parts of the network and how.
-        The syntax is:
-            SCHEDULE -> mINT:cINT
-            INT -> {1..9}{0..9}*
-
-        where 'm' and 'c' specify the network part to be trained:
-            - m [model], train the main model
-            - c [classifiers], train the attached classifiers (y and s)
-
-        the integer after the 'm' option specifies the total number of epochs to be performed;
-        the integer after the 'c' option specifies how much the sensible and y layer have to be trained
-        at each iteration.
-
-        example:
-            m100:c100        -- train the whole network for 10 epochs; for each batch train
-                                the sensible and y network for 100 iterations before using it to
-                                feedback the prediction to train model
-
-        "*_LAYERS" options specify the composition of sub networks;
-        syntax is:
-            LAYER -> [sl]?INT
-            LAYER -> [sl]?INT:LAYER
-
-        where the integers are the number of hidden units in the layer being specified and the
-        optional 's' or 'l' flags specify the activation unit to be used (s==sigmoid, l==linear,
-        default=='s').
-
-        examples:
-             10       -- a single layer with 10 sigmoid neurons
-             10:5:2   -- three layers with 10, 5, and 2 neuron respectively, allo sigmoid units
-             l5:s3    -- two layers, first has 5 linear units, the second has 3 sigmoid units
-
-        NOTE: layers options CAN be omitted when the model is restored from a .ckpt file
-        """
-
-        config_opts = self.try_load_opts(argv)
-
-        datasets = { 'adult': AdultDataset, 'bank': BankMarketingDataset, 'german':GermanDataset, 'synth': SynthDataset }
-        parser = argparse.ArgumentParser(description=description,formatter_class=argparse.RawDescriptionHelpFormatter)
-        parser.add_argument('-c', '--checkpoint', metavar="FILENAME.ckpt", required=not 'checkpoint' in config_opts, type=str, help="Name of the checkpoint to be saved/loaded.")
+    def configure_parser(self, parser, checkpoint_already_given=None, dataset_already_given=None):
+        parser.add_argument('-c', '--checkpoint', metavar="FILENAME.ckpt", required=not checkpoint_already_given, type=str, help="Name of the checkpoint to be saved/loaded.")
         parser.add_argument('-o', '--output', metavar="FILENAME.ckpt", type=str, help="Name of the checkpoint to be saved into. Defaults to the value given to the -c parameter if not given.")
         parser.add_argument('-H', '--hidden-layers', type=str, help='hidden layers specs')
         parser.add_argument('-S', '--sensible-layers', type=str, help='sensible network specs')
@@ -198,8 +241,16 @@ class Options:
         parser.add_argument('-f', '--fairness-importance', type=float, help="Specify how important is fairness w.r.t. the error")
         parser.add_argument('-d', '--dataset-base-path', type=str, help="Specify the base directory for storing and reading datasets")
 
-        if not 'dataset' in config_opts:
+        if not dataset_already_given:
             parser.add_argument('dataset', choices=['adult', 'bank', 'german', 'synth'], help="dataset to be loaded")
+
+    def parse(self, argv):
+        config_opts = self.try_load_opts(argv)
+
+        datasets = { 'adult': AdultDataset, 'bank': BankMarketingDataset, 'german':GermanDataset, 'synth': SynthDataset }
+        parser = argparse.ArgumentParser(description=PARAMS_DESCRIPTION, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+        self.configure_parser(parser, checkpoint_already_given='checkpoint' in config_opts, dataset_already_given='dataset' in config_opts)
 
         result = self.try_update_opts(config_opts, parser.parse_args(argv[1:]))
 
