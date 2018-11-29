7773d5aa1b183d54bc8b83e0323dad12791c22d1
boborbt <boborbt@gmail.com>
2018-11-29 18:08:29 +0100

    Support launching the build_representation script from root directory

--- code/build_representations.py
+++ code/build_representations.py
@@ -2,6 +2,14 @@ import os
 import sys
 from termcolor import colored
 import subprocess
+import json
+
+
+def extract_model_path(config_path):
+    config_string = open(config_path, "r").read()
+    config = json.loads(config_string)
+    return config["checkpoint"]
+
 
 code_path = sys.argv[1]
 experiments_path = sys.argv[2]
@@ -13,11 +21,12 @@ for dir in os.listdir(experiments_path):
         print(colored("\tBuilding representations with approach %s" % approach, "yellow"))
         try:
             script_path = os.path.join(code_path, approach) + ".py"
-            model_path = os.path.join(experiments_path, dir, "models", "model.ckpt")
             output_path = os.path.join(experiments_path, dir, "representations", "%s_repr.csv" % approach)
             config_path = os.path.join(experiments_path, dir, "config.json")
 
             if os.path.exists(config_path):
+                model_path = os.path.join(experiments_path, dir, extract_model_path(config_path))
+
                 cmd = ["python", script_path, config_path, "-E", output_path, "-c", model_path]
                 subprocess.check_output(cmd)
             else:
