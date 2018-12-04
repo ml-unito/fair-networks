ecd51abc170e3d629a9d4e5d3d119aa48963d162
Roberto Esposito <roberto.esposito@unito.it>
2018-12-04 18:11:58 +0100

    Completed new test_networks

--- code/test_network.py
+++ code/test_network.py
@@ -9,16 +9,17 @@ sys.path.append('code')
 
 EPOCHS = 100
 BATCH_SIZE = 100
+RANDOM_SEED = 42
 
 
 class Model:
     def __init__(self, num_features, num_s):
-        self.x = tf.placeholder(tf.float, shape=[None, input_size])
-        self.s = tf.placeholder(tf.float, shape=[None, num_s])
+        self.x = tf.placeholder(tf.float32, shape=[None, num_features])
+        self.s = tf.placeholder(tf.float32, shape=[None, num_s])
 
-        self.in_layer = x
-        self.h_layer = tf.layers.dense(in_layer, 10)
-        self.out = tf.layers.dense(h_layer, num_s)
+        self.in_layer = self.x
+        self.h_layer = tf.layers.dense(self.in_layer, 10, kernel_initializer=tf.truncated_normal_initializer())
+        self.out = tf.layers.dense(self.h_layer, num_s, kernel_initializer=tf.truncated_normal_initializer())
 
         self.loss = tf.losses.sigmoid_cross_entropy(self.s, self.out)
 
@@ -35,39 +36,26 @@ h_columns = df.filter(regex=("h.*")).values
 h_train, h_test, s_train, s_test, y_train, y_test = ms.train_test_split(
     h_columns, s_columns, y_columns, random_state=RANDOM_SEED)
 
+
+model = Model(h_columns.shape[1], s_columns.shape[1])
+
+optimizer = tf.train.AdagradOptimizer(1.0)
+train_step = optimizer.minimize(model.loss)
+
 init = tf.global_variables_initializer()
 session = tf.Session()
-
 session.run(init)
 
-for epoch in len(range(EPOCHS)):
+for epoch in range(EPOCHS):
     batch_pos = 0
     while batch_pos < len(h_train):
-        batch = h_train[batch_pos:(batch_pos+BATCH_SIZE)]
+        batch_x = h_train[batch_pos:(batch_pos+BATCH_SIZE)]
+        batch_s = s_train[batch_pos:(batch_pos+BATCH_SIZE)]
         batch_pos += BATCH_SIZE
 
+        session.run(train_step, feed_dict={model.x: batch_x, model.s: batch_s })
 
+    print(session.run(model.loss, feed_dict={model.x: batch_x, model.s: batch_s }))
 
-
-
-
-svc_y.fit(h_train, y_train)
-pred_y_train = svc_y.predict(h_train)
-pred_y_test = svc_y.predict(h_test)
-
-acc_y_train = sum(pred_y_train == y_train)/float(len(y_train))
-acc_y_test = sum(pred_y_test == y_test)/float(len(y_test))
-
-svc_s = svm.SVC(random_state=RANDOM_SEED)
-
-svc_s.fit(h_train, s_train)
-
-pred_s_train = svc_s.predict(h_train)
-pred_s_test = svc_s.predict(h_test)
-
-acc_s_train = sum(pred_s_train == s_train)/float(len(s_train))
-acc_s_test = sum(pred_s_test == s_test)/float(len(s_test))
-
-return (acc_s_train, acc_y_train, acc_s_test, acc_y_test)
 
 
