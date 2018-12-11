1f829172bb9292ebe88fbce0257fa152aa1ed81e
boborbt <boborbt@gmail.com>
2018-12-11 18:07:54 +0100

    Code cleanup and refactoring

--- bin/test_representations.py
+++ bin/test_representations.py
@@ -30,102 +30,59 @@ def read_commit_id(file_path):
     else:
         return "N/A"
 
+def accuracy(pred, y):
+    return np.average(y == pred)
+
 def eval_accuracies_on_representation(file):
+    print("processing: {}".format(file))
     df = pandas.read_csv(file)
     y_columns = df.filter(regex=("y.*")).values
     s_columns = df.filter(regex=("s.*")).values
     h_columns = df.filter(regex=("h.*")).values
 
     h_train, h_test, s_train, s_test, y_train, y_test = ms.train_test_split(h_columns, s_columns, y_columns, random_state=RANDOM_SEED)
+    
 
     y_train = np.argmax(y_train, axis=1)
     y_test = np.argmax(y_test, axis=1)
     s_train = np.argmax(s_train, axis=1)
     s_test = np.argmax(s_test, axis=1)
 
+    datasets = (h_train, y_train, h_test, y_test, s_train, s_test)
+
     results = {}
-    results["svc"] = svc_performances(h_train, y_train, h_test, y_test, s_train, s_test)
-    results["tree"] = trees_performances(h_train, y_train, h_test, y_test, s_train, s_test)
-    results["lr"] = logistic_regression_performances(h_train, y_train, h_test, y_test, s_train, s_test)
+    results["svc"] = test_performances(klass=svm.SVC, kargs={"random_state": RANDOM_SEED, "kernel":"linear"}, datasets=datasets)
+    results["tree"] = test_performances(klass=tree.DecisionTreeClassifier, kargs={"random_state":RANDOM_SEED, "max_depth":4}, datasets=datasets )
+    results["lr"] = test_performances(klass=lm.LogisticRegression, kargs={"random_state": RANDOM_SEED}, datasets=datasets)
     return results
 
 
-def logistic_regression_performances(h_train, y_train, h_test, y_test, s_train, s_test):
-    logistic_regression_y = lm.LogisticRegression(random_state=RANDOM_SEED)
+def test_performances(klass, kargs, datasets):
+    h_train, y_train, h_test, y_test, s_train, s_test = datasets
+    classifier_y = klass(**kargs)
 
-    logistic_regression_y.fit(h_train, y_train)
-    pred_y_train = logistic_regression_y.predict(h_train)
-    pred_y_test = logistic_regression_y.predict(h_test)
+    classifier_y.fit(h_train, y_train)
+    pred_y_train = classifier_y.predict(h_train)
+    pred_y_test = classifier_y.predict(h_test)
 
-    acc_y_train = sum(pred_y_train == y_train)/float(len(y_train))
-    acc_y_test = sum(pred_y_test == y_test)/float(len(y_test))
+    acc_y_train = accuracy(pred_y_train, y_train)
+    acc_y_test = accuracy(pred_y_test, y_test)
 
-    logistic_regression_s = lm.LogisticRegression(random_state=RANDOM_SEED)
+    classifier_s = klass(**kargs)
 
-    logistic_regression_s.fit(h_train, s_train)
+    classifier_s.fit(h_train, s_train)
 
-    pred_s_train = logistic_regression_s.predict(h_train)
-    pred_s_test = logistic_regression_s.predict(h_test)
+    pred_s_train = classifier_s.predict(h_train)
+    pred_s_test = classifier_s.predict(h_test)
 
-    acc_s_train = sum(pred_s_train == s_train)/float(len(s_train))
-    acc_s_test = sum(pred_s_test == s_test)/float(len(s_test))
+    acc_s_train = accuracy(pred_s_train, s_train)
+    acc_s_test = accuracy(pred_s_test, s_test)
 
     return  {
         "s": {"train": acc_s_train, "test": acc_s_test},
         "y": {"train": acc_y_train, "test": acc_y_test}
         }
 
-
-def svc_performances(h_train, y_train, h_test, y_test, s_train, s_test):
-    svc_y = svm.SVC(random_state=RANDOM_SEED)
-
-    svc_y.fit(h_train, y_train)
-    pred_y_train = svc_y.predict(h_train)
-    pred_y_test = svc_y.predict(h_test)
-
-    acc_y_train = sum(pred_y_train == y_train)/float(len(y_train))
-    acc_y_test = sum(pred_y_test == y_test)/float(len(y_test))
-
-    svc_s = svm.SVC(random_state=RANDOM_SEED)
-
-    svc_s.fit(h_train, s_train)
-
-    pred_s_train = svc_s.predict(h_train)
-    pred_s_test = svc_s.predict(h_test)
-
-    acc_s_train = sum(pred_s_train == s_train)/float(len(s_train))
-    acc_s_test = sum(pred_s_test == s_test)/float(len(s_test))
-
-    return {
-        "s": {"train": acc_s_train, "test": acc_s_test},
-        "y": {"train": acc_y_train, "test": acc_y_test } 
-        }
-
-
-def trees_performances(h_train, y_train, h_test, y_test, s_train, s_test):
-    tree_y = tree.DecisionTreeClassifier(max_depth=4, random_state=RANDOM_SEED)
-
-    tree_y.fit(h_train, y_train)
-    pred_y_train = tree_y.predict(h_train)
-    pred_y_test = tree_y.predict(h_test)
-
-    acc_y_train = sum(pred_y_train == y_train)/float(len(y_train))
-    acc_y_test = sum(pred_y_test == y_test)/float(len(y_test))
-
-    tree_s = tree.DecisionTreeClassifier(max_depth=4, random_state=RANDOM_SEED)
-
-    tree_s.fit(h_train, s_train)
-
-    pred_s_train = tree_s.predict(h_train)
-    pred_s_test = tree_s.predict(h_test)
-
-    acc_s_train = sum(pred_s_train == s_train)/float(len(s_train))
-    acc_s_test = sum(pred_s_test == s_test)/float(len(s_test))
-
-    return {
-        "s": {"train": acc_s_train, "test": acc_s_test},
-        "y": {"train": acc_y_train, "test": acc_y_test}
-        }
 
 def process_dir(path):
     config_path = os.path.join(path, "config.json")
