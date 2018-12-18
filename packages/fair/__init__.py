24f256ccad913cfb6b819df357f21d9340aff96c
boborbt <boborbt@gmail.com>
2018-12-18 18:53:31 +0100

    Adds __init__.py files for submodules

---
+++ packages/fair/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module implements fair-networks.
+
+It is composed of several sub-modules:
+   fn: main implementation of the model (the model itself and the training procedure)
+   rn: "random-network" implementation (one that output a random representation)
+   utils: utility classes (mainly, the Options class)
+   vfae: "variational fair autoencoders" implementation
+"""
