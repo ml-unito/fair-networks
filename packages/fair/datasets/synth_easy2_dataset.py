3addd6a5f35492f40dce33c8eb9f85be117c7a9a
boborbt <boborbt@gmail.com>
2019-01-16 15:36:36 +0100

    Synth easy 3 (and correction to synth easy 2)

--- packages/fair/datasets/synth_easy2_dataset.py
+++ packages/fair/datasets/synth_easy2_dataset.py
@@ -1,5 +1,8 @@
 from .synth_easy_dataset import SynthEasyDataset
 
 class SynthEasy2Dataset(SynthEasyDataset):
+    def name(self):
+        return "SynthEasy2"
+
     def dataset_path(self):
         return 'data/synth-easy2.csv'
